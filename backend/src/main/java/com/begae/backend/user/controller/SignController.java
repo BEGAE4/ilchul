643c55b0dc98dc1f4aa092a55f7634102c106c20
JPhong <hsc3757@gmail.com>
2025-08-13 21:30:25 +0900

    feat: HttpOnly 쿠키를 통한 로그인 확인 메서드 구현

--- backend/src/main/java/com/begae/backend/user/controller/SignController.java
+++ backend/src/main/java/com/begae/backend/user/controller/SignController.java
@@ -1,12 +1,15 @@
 package com.begae.backend.user.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
+import com.begae.backend.user.auth.OauthUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,6 +29,17 @@ public class SignController {
     @GetMapping("/login/naver")
     public ResponseEntity<String> loginNaver() {
         return new ResponseEntity<>("네이버 로그인 성공", HttpStatus.OK);
+    }
+
+    @GetMapping("/userinfo")
+    public ResponseEntity<?> userInfo(@AuthenticationPrincipal OauthUserDetails oauthUserDetails) {
+        if(oauthUserDetails == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        return ResponseEntity.ok(Map.of(
+                "email", oauthUserDetails.getName(),
+                "role", oauthUserDetails.getAuthorities()
+        ));
     }
 
     @GetMapping("/reissue")
