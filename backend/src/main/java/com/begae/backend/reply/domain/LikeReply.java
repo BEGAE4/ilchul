af201a2d91a56b2e5757dc3fbf72a9338b8bfcc3
장욱 <janguk95@naver.com>
2026-05-11 17:53:30 +0900

    fix: 댓글 좋아요 엔티티 추가

---
+++ backend/src/main/java/com/begae/backend/reply/domain/LikeReply.java
@@ -0,0 +1,46 @@
+package com.begae.backend.reply.domain;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.begae.backend.global.domain.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "like_reply")
+public class LikeReply extends BaseEntity {
+    
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "like_reply_id")
+    private Integer likeReplyId;
+
+    @Column(name = "user_id")
+    private Integer userId;
+
+    @Column(name = "reply_id")
+    private Integer replyId;
+
+    public static LikeReply of(Integer userId, Integer replyId) {
+        return new LikeReply(userId, replyId);
+    }
+
+    private LikeReply(Integer userId, Integer replyId) {
+        this.userId = userId;
+        this.replyId = replyId;
+    } 
+}
