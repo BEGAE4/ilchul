0035e15d82a6e61d4a4d9b6ddb1f553425a4fe7f
장욱 <janguk95@naver.com>
2026-05-31 18:40:25 +0900

    feat: 문의 관련 엔티티 생성 및 갱신

---
+++ backend/src/main/java/com/begae/backend/cs_inquiry/domain/CsInquiryImage.java
@@ -0,0 +1,38 @@
+package com.begae.backend.cs_inquiry.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "cs_inquiry_image")
+public class CsInquiryImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "image_id")
+    private Integer imageId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "inquiry_id")
+    private CsInquiry csInquiry;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    private CsInquiryImage(CsInquiry csInquiry, String imageUrl) {
+        this.csInquiry = csInquiry;
+        this.imageUrl = imageUrl;
+    }
+
+    public static CsInquiryImage of(CsInquiry csInquiry, String imageUrl) {
+        return new CsInquiryImage(csInquiry, imageUrl);
+    }
+
+    public void setCsInquiry(CsInquiry csInquiry) {
+        this.csInquiry = csInquiry;
+    }
+}
