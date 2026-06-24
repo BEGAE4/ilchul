c6ae754be9fa639a821b1604c0823d38f5edba45
JPhong <hsc3757@gmail.com>
2026-06-24 18:11:59 +0900

    refactor: cors 설정 중복으로 사용되지 않는 설정 삭제

--- backend/src/main/java/com/begae/backend/config/WebConfig.java
+++ backend/src/main/java/com/begae/backend/config/WebConfig.java
@@ -1,18 +1,9 @@
 package com.begae.backend.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowedHeaders("*")
-                .allowedMethods("*")
-                .allowCredentials(true)
-                .maxAge(3600);
-    }
+    // cors 설정 중복으로 삭제
 }
